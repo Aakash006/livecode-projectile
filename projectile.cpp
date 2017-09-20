6d29d9a243cbd5e0bf24558f7e35aa0d0b9934f4
Aakash006 <aakashpatel141@gmail.com>
2017-09-20 13:56:38 -0400

    added code
    added objects

---
+++ projectile.cpp
@@ -0,0 +1,10 @@
+#include "projectile.hpp"
+#include <cstdio>
+
+const double m = 0;
+const double c = 1;
+
+auto force(TState s){
+	return VecR2{0, -m*g};
+}
+
--- projectile.hpp
+++ projectile.hpp
@@ -1,3 +1,32 @@
 #pragma once
 
 #include <cstdio>
+
+struct {
+	double x{0};
+	double y{0};
+};
+auto operator + (VecR2 a, VecR2 b){
+	return VecR2{a.x + b.x, a.y + b.y}
+}
+
+auto operator + (VecR2 a, double c){
+	return VecR2{c + a.x, c + a.x};
+}
+
+auto operator + (double c, VecR2 a){
+	return a + c;
+}
+
+struct TState{
+	double t{0};
+	VecR2 position{0,0};
+	VecR2 velocity{0,0};
+};
+
+void print_tstate{
+
+}
+
+
+
